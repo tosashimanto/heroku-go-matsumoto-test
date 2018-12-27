e745266f8a04ed59585b77a8fdbcba8060f695c8
Yoshimasa MATSUMOTO <tosashimanto@gmail.com>
2018-12-27 21:55:40 +0900

    CircelCiテスト.

--- main.go
+++ main.go
@@ -3,5 +3,7 @@ package main
 import "github.com/tosashimanto/heroku-go-matsumoto-test/controller"
 
 func main() {
+
 	controller.Handler()
+
 }
--- model/test001.go
+++ model/test001.go
@@ -1,11 +1,13 @@
 package model
 
+// bbbb
 type Test001 struct {
-	Id             int64     `json:"testId"`
+	ID             int64     `json:"testId"`
 	RegisteredDate string    `json:"registeredDate"`
 	TestArray      []Test002 `json:"testArray"`
 }
 
+// aaaa
 type Test002 struct {
 	Index        int64 `json:"index"`
 	JudgedNumber int64 `json:"number"`
